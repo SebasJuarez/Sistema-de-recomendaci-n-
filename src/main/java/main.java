6c54f1fbc412ecc61932cc742e1ed4b636e3f697
AleMar21430 <mar21430@uvg.edu.gt>
2022-05-26 11:10:08 -0600

    Local_Test

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -5,6 +5,7 @@ public class main {
     public static void main(String[] args) {
         GUI Interfaz= new GUI();
         operations OP = new operations();
+        database_parser DBP = new database_parser();
         local_database DB = new local_database();
         Interfaz.show();
 
