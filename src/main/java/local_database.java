09e7d17ab0a55822530ec9474bc0e3f411e15a17
AleMar21430 <mar21430@uvg.edu.gt>
2022-05-27 15:23:04 -0600

    impots

--- src/main/java/local_database.java
+++ src/main/java/local_database.java
@@ -1,7 +1,3 @@
-
-
-import java.util.ArrayList;
-
 public class local_database {
     public String[] getCarManufacturers(){
         String[] Manufacturers = {"Alfa Romeo", "Aston Martin", "Bentley", "Audi", "BMW", "Bugatti", "BYD", "Cadillac", "Changan", "Chevrolet", "Chrysler", "Datsun", "Dodge", "Ferrari", "Ford", "GMC", "Honda", "Hummer", "Hyundai", "Isuzu", "Jaguar", "Jeep", "Kia", "Lamborghini", "Land Rover", "Lexus", "Maserati", "Mazda", "McLaren", "Mercedes-Benz", "Mitsubishi", "Nikola", "Nissan", "Opel", "Peugeot", "Pontiac", "Porsche", "RAM", "Renault", "Rolls-Royce", "Skoda", "Subaru", "Suzuki", "Toyota", "Volkswagen", "Volvo", "W Motors"};
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,6 +1,3 @@
-import java.io.*;
-import java.util.*;
-
 public class main {
         
     public static void main(String[] args) {
