26b95ab957c55887a3001209fe8a0a5f237fcbca
Sebastian Juárez <84484212+SebasJuarez@users.noreply.github.com>
2022-06-04 22:25:52 -0600

    Delete src/main/java directory

--- src/main/java/database_parser.java
+++
@@ -1,3 +0,0 @@
-public class database_parser {
-    
-}
--- src/main/java/operations.java
+++
@@ -1,9 +0,0 @@
-public class operations {
-    public int Int_MapRange(float from_A, float from_B, float to_A, float to_B, float X){
-        int Value = (int) (to_A + ((X - from_A)*(to_B - to_A))/(from_B - from_A));
-        return Value;
-    }
-    public int Int_Clamp(int val, int min, int max) {
-    return Math.max(min, Math.min(max, val));
-}
-}
